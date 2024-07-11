061e17c4b43d949226870d7faff166d49965ad2a
vitoinacio <victor.hugo.ina10@gmail.com>
2024-07-10 21:31:02 -0300

    primeiro commit

---
+++ src/js/modules/accordion.js
@@ -0,0 +1,15 @@
+export default function initAccordion() {
+  const accordionList = document.querySelectorAll(
+    "[data-anime='accordion'] dt"
+  );
+  if (accordionList.length) {
+    function activeAccordion() {
+      this.classList.toggle("ativo");
+      this.nextElementSibling.classList.toggle("ativo");
+    }
+
+    accordionList.forEach((item) => {
+      item.addEventListener("click", activeAccordion);
+    });
+  }
+}
