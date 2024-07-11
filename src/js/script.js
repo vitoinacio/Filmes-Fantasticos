759928b72bd6ff66c4fb045f457b596533f47630
Victor Hugo <victor.hugo.ina10@gmail.com>
2024-07-11 19:52:19 -0300

    posicao anima-scroll

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,19 @@
 import ScrollSuave from "./modules/scroll-suave.js";
-import initAnimaScroll from "./modules/scroll-animacao.js";
 import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tabnav.js";
 import initModal from "./modules/modal.js";
 import initToolTip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
+import initAnimaScroll from "./modules/scroll-animacao.js";
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
 
-initAnimaScroll();
 initAccordion();
 initTabNav();
 initModal();
 initToolTip();
 initDropdownMenu();
 initMenuMobile();
+initAnimaScroll();
