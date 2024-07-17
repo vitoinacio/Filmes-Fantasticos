07761613d0bd3fe81217c3584ea94bcb86037e18
Victor Hugo <victor.hugo.ina10@gmail.com>
2024-07-17 13:04:51 -0300

    animacao ao scroll refeita

--- src/js/modules/scroll-anima.js
+++ src/js/modules/scroll-anima.js
@@ -1,26 +1,45 @@
 export default class ScrollAnima {
   constructor(sections){
     this.sections = document.querySelectorAll(sections);
-    this.windowMetade = window.innerHeight * 0.8;
+    this.windowMetade = window.innerHeight * 0.6;
 
-    this.animaScroll = this.animaScroll.bind(this);
+    this.checkDistance = this.checkDistance.bind(this);
   }
 
-  animaScroll() {
-    this.sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top;
-      const issectionVisible = (sectionTop - this.windowMetade) < 0;
-      if (issectionVisible) {
-        section.classList.add("ativo");
-      } else if (section.classList.contains("ativo")) {
-        section.classList.remove("ativo");
+  //pega a distancia de cada item em 
+  //relacao ao topo do site
+  getDistance(){
+    this.distance = [...this.sections].map((section)=>{
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        offset: Math.floor(offset - this.windowMetade),
+      };
+    });
+  }
+
+  // verifica a distancia em cada objeto 
+  // em relaçao ao scroll do site 
+  checkDistance(){
+    this.distance.forEach((item)=>{
+      if (window.scrollY > item.offset) {
+        item.element.classList.add('ativo')
+      } else if (item.element.classList.contains("ativo")) {
+        item.element.classList.remove("ativo");
       }
     });
   }
 
     init(){
-      this.animaScroll();
-      window.addEventListener("scroll", this.animaScroll);
+      if (this.sections.length) {
+        this.getDistance();
+        this.checkDistance();
+        window.addEventListener("scroll", this.checkDistance);
+      }
       return this
+    }
+
+    stop(){
+      window.removeEventListener("scroll", this.checkDistance);
     }
 }
